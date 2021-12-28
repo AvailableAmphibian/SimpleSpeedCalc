7cdba5c7b6cf82bb963b50e425071fa16c526a96
Dra <56563878+HFDrae@users.noreply.github.com>
2021-12-28 23:31:55 +0100

    Add the file and the README ! (#1)
    Everything that we need !

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SimpleSpeedCalc
-Simple speed tune calculator for a Jamire LightHomunculus Lushen setup for Summoners War in Python
+Simple speed tune calculator for a Jamire LightHomunculus Lushen setup for Summoners War in Python.
+
+You can execute the code [here](https://replit.com/@HFDrae/SquareCreepyVariety#main.py), you just need to modify the last line to match your needs !
+
+Don't hesitate to contact me if you need an explanation or found a mistake !
---
+++ speed_calc.py
@@ -0,0 +1,60 @@
+# === Constants ===
+
+# Final multiplier
+SPD_BUFF = 1.3
+# Base Multipliers
+TOTEM = 0.15
+LEAD = 0.24
+# Base speed
+LUSHEN_BASE_SPD = 103
+HOMUNL_BASE_SPD = 101
+JAMIRE_BASE_SPD = 113
+
+# =================
+
+# === Functions ===
+
+def calcSpeedTune(jamire_bonus_speed, use_lead):
+    """
+    Parameters:
+        jamire_bonus_speed (int): The bonus speed of Jamire's rune.
+        use_lead (bool): If the lead speed should be used or not.
+    
+    Returns:
+        ((int, int), (int,int)) the speed that Lushen and the Homunculus need to be perfectly speed tuned.
+        The couples are as followed : ((lushen_total, lushen_bonus), (homun_total, homun_bonus))
+    """
+    
+    base_mult = TOTEM
+    if use_lead:
+        base_mult += LEAD
+    
+    JAMIRE_ADDED_SPEED = JAMIRE_BASE_SPD * (1 + base_mult)
+    jamire_final_speed = JAMIRE_ADDED_SPEED + jamire_bonus_speed
+    
+    LUSHEN_BASE_BONUS = LUSHEN_BASE_SPD * (1 + base_mult)
+    
+    homunl_total = int( jamire_final_speed - 1 - (HOMUNL_BASE_SPD * base_mult) ) - 1
+    homunl_rune_bonus = homunl_total - HOMUNL_BASE_SPD
+    
+    homunl_return = (homunl_total, homunl_rune_bonus)
+    
+    lushen_rune_bonus = 0
+    
+    while (LUSHEN_BASE_BONUS + lushen_rune_bonus) * SPD_BUFF <= jamire_final_speed:
+        lushen_rune_bonus += 1
+        
+    lushen_total = LUSHEN_BASE_SPD + lushen_rune_bonus
+    
+    lushen_return = (lushen_total, lushen_rune_bonus)
+    
+    return (lushen_return, homunl_return)
+
+def display_for_it(jamire_bonus_speed, use_lead):
+    ((lu_t, lu_rb), (ho_t, ho_rb)) = calcSpeedTune(jamire_bonus_speed, use_lead)
+    print("You need +"+ str(ho_rb) + " speed on your light homun to reach " + str(ho_t) + " speed.")
+    print("You need +" + str(lu_rb) +" speed on your Lushen to reach " + str(lu_t) + " speed.")
+    
+# =================
+
+display_for_it(200, False)
