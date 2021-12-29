3d17624d0bcb7bf2797ccf5ada073275460949e2
Dra <56563878+HFDrae@users.noreply.github.com>
2021-12-29 11:25:20 +0100

    Update speed_calc.py

--- speed_calc.py
+++ speed_calc.py
@@ -25,29 +25,34 @@ def calcSpeedTune(jamire_bonus_speed, use_lead):
         The couples are as followed : ((lushen_total, lushen_bonus), (homun_total, homun_bonus))
     """
     
-    base_mult = TOTEM
+    base_mult = TOTEM # = 0.15
     if use_lead:
-        base_mult += LEAD
+        base_mult += LEAD # = 0.39
     
-    JAMIRE_ADDED_SPEED = JAMIRE_BASE_SPD * (1 + base_mult)
-    jamire_final_speed = JAMIRE_ADDED_SPEED + jamire_bonus_speed
+    # --- jamire ---
+    JAMIRE_ADDED_SPEED = JAMIRE_BASE_SPD * (1 + base_mult) # = 113 * 1.39
+    jamire_final_speed = JAMIRE_ADDED_SPEED + jamire_bonus_speed # = BASE * 1.39 + runes
+    # --------------
     
-    LUSHEN_BASE_BONUS = LUSHEN_BASE_SPD * (1 + base_mult)
-    
-    homunl_total = int( jamire_final_speed - 1 - (HOMUNL_BASE_SPD * base_mult) ) - 1
-    homunl_rune_bonus = homunl_total - HOMUNL_BASE_SPD
+    # --- homunl ---
+    homunl_total = int( jamire_final_speed - 1 - (HOMUNL_BASE_SPD * base_mult) ) - 1 # = jamire final - homun lead+totem bonus
+    homunl_rune_bonus = homunl_total - HOMUNL_BASE_SPD # only runes
     
     homunl_return = (homunl_total, homunl_rune_bonus)
+    # --------------
     
-    lushen_rune_bonus = 0
+    # --- Lushen ---
+    LUSHEN_BASE_BONUS = LUSHEN_BASE_SPD * (1 + base_mult) # 103 * 1.39
+    lushen_rune_bonus = 0 # current rune bonus needed
     
     while (LUSHEN_BASE_BONUS + lushen_rune_bonus) * SPD_BUFF <= jamire_final_speed:
-        lushen_rune_bonus += 1
+        lushen_rune_bonus += 1 # calculating the minimum speed required from runes
         
-    lushen_total = LUSHEN_BASE_SPD + lushen_rune_bonus
+    lushen_total = LUSHEN_BASE_SPD + lushen_rune_bonus # readable total (103 + rune bonus)
     
     lushen_return = (lushen_total, lushen_rune_bonus)
-    
+    # --------------
+
     return (lushen_return, homunl_return)
 
 def display_for_it(jamire_bonus_speed, use_lead):
@@ -57,4 +62,5 @@ def display_for_it(jamire_bonus_speed, use_lead):
     
 # =================
 
-display_for_it(200, False)
+# Here we calc for a Jamire with a bonus of 200 from runes and using it's lead
+display_for_it(200, True)
