ac0ddd1fa039f86c8abdea411785b0057abdb39d
Dra <56563878+HFDrae@users.noreply.github.com>
2021-12-29 21:27:59 +0100

    Feature : Use whatever monster you want (#3)
    * Update speed_calc.py
    
    Add the nuker as a parameter and Lushen as default.
    
    * Update README.md
    
    Fix a mistake, add the default
    
    * Update speed_calc.py
    
    Change names

--- README.md
+++ README.md
@@ -11,17 +11,25 @@ This calculator is used with the Light Homunculus' S3 : `Fast Pace`.
 
 The speedtune aimed is the followed :
 - Jamire plays (s1 or s2)
-- HomunLight casts S3 on Lushen -> *atb resets and is set to `100` thanks to `Fast Pace`*
+- HomunLight casts S3 on Lushen ⇾ *atb resets and is set to `100` thanks to `Fast Pace`*
 - Lushen casts S3
 - Jamire casts S3
-- HomunLight casts S3 on Lushen -> *atb resets and is set to `100` thanks to `Fast Pace`* (again)
+- HomunLight casts S3 on herself ⇾ *atb resets and is set to `100` thanks to `Fast Pace`* (again)
+- HomunLight casts S2
 - Lushen casts S3 (again)
  
 ### How to use it ?
 
-You can execute the script [here](https://replit.com/@HFDrae/SquareCreepyVariety#main.py), you just need to modify the last line to match your needs !
+You can execute the script [here](https://replit.com/@HFDrae/SquareCreepyVariety#main.py), you just need to modify the
+last line to match your needs !
 
-You also have the possibility to clone the project or only download it and execute it on your computer using the following command :
+> If you want to use another monster, you need to provide the base speed with the following pattern :
+> ```python
+> display_needed_speed(200, True, nuker_bs=120) # change the 120 to match your needs !
+> ```
+
+You also have the possibility to clone the project or only download it and execute it on your computer using the
+following command :
 Windows (cmd) :
 ```
 > python3 path\to\file\speed_calc.py
@@ -43,14 +51,22 @@ Ce calculator est utilisé avec le S3 de l'Homunculus Lumière : `Rythme Rapide`
 
 On va viser le speed tune suivant :
 - Jamire joue (s1 or s2)
-- HomunLight fait S3 sur Lushen -> *on met tous les atb à `100` grâce à `Rythme Rapide`*
+- HomunLight fait S3 sur Lushen ⇾ *on met tous les atb à `100` grâce à `Rythme Rapide`*
 - Lushen fait S3
-- Jamire fait S3 -> HomunLight fait S3 sur Lushen -> *on met tous les atb à `100` grâce à `Rythme Rapide`* (encore)
+- Jamire fait S3 
+- HomunLight fait S3 sur elle ⇾ *on met tous les atb à `100` grâce à `Rythme Rapide`* (encore)
+- HomunLight fait S2
 - Lushen fait S3 (encore)
 
 ### Comment l'utiliser ?
 
-Vous avez la possibilité de lancer le programme d'[ici](https://replit.com/@HFDrae/SquareCreepyVariety#main.py), il faudra juste modifier la dernière ligne du programme pour obtenir le résultat voulu !
+Vous avez la possibilité de lancer le programme d'[ici](https://replit.com/@HFDrae/SquareCreepyVariety#main.py),
+il faudra juste modifier la dernière ligne du programme pour obtenir le résultat voulu !
+
+> Si vous voulez utiliser un autre monstre, vous avez besoin de fournir la base speed de celui-ci en suivant la ligne suivante :
+> ```python
+> display_needed_speed(200, True, nuker_bs=120) # Changez le 120 selon vos besoins !
+> ```
 
 Vous avez aussi la possibilité de cloner le projet ou simplement le télécharger et l'éxécuter sur votre machine en utilisant la commande suivante :
 ```
--- speed_calc.py
+++ speed_calc.py
@@ -6,61 +6,65 @@ SPD_BUFF = 1.3
 TOTEM = 0.15
 LEAD = 0.24
 # Base speed
-LUSHEN_BASE_SPD = 103
+LUSHEN_BASE_SPD = 103  # Base monster used
 HOMUNL_BASE_SPD = 101
 JAMIRE_BASE_SPD = 113
+
 
 # =================
 
 # === Functions ===
 
-def calcSpeedTune(jamire_bonus_speed, use_lead):
+def calc_speed_tune(jamire_bonus_speed, use_lead, nuker_base_speed):
     """
     Parameters:
         jamire_bonus_speed (int): The bonus speed of Jamire's rune.
         use_lead (bool): If the lead speed should be used or not.
-    
+        nuker_base_speed (int): The base speed of the nuker used, default is Lushen's
     Returns:
         ((int, int), (int,int)) the speed that Lushen and the Homunculus need to be perfectly speed tuned.
         The couples are as followed : ((lushen_total, lushen_bonus), (homun_total, homun_bonus))
     """
-    
-    base_mult = TOTEM # = 0.15
+
+    base_mult = TOTEM  # = 0.15
     if use_lead:
-        base_mult += LEAD # = 0.39
-    
+        base_mult += LEAD  # = 0.39
+
     # --- jamire ---
-    JAMIRE_ADDED_SPEED = JAMIRE_BASE_SPD * (1 + base_mult) # = 113 * 1.39
-    jamire_final_speed = JAMIRE_ADDED_SPEED + jamire_bonus_speed # = BASE * 1.39 + runes
+    JAMIRE_ADDED_SPEED = JAMIRE_BASE_SPD * (1 + base_mult)  # = 113 * 1.39
+    jamire_final_speed = JAMIRE_ADDED_SPEED + jamire_bonus_speed  # = BASE * 1.39 + runes
     # --------------
-    
+
     # --- homunl ---
-    homunl_total = int( jamire_final_speed - 1 - (HOMUNL_BASE_SPD * base_mult) ) - 1 # = jamire final - homun lead+totem bonus
-    homunl_rune_bonus = homunl_total - HOMUNL_BASE_SPD # only runes
-    
+    homunl_total = int(
+        jamire_final_speed - 1 - (HOMUNL_BASE_SPD * base_mult)) - 1  # = jamire final - homun lead+totem bonus
+    homunl_rune_bonus = homunl_total - HOMUNL_BASE_SPD  # only runes
+
     homunl_return = (homunl_total, homunl_rune_bonus)
     # --------------
-    
-    # --- Lushen ---
-    LUSHEN_BASE_BONUS = LUSHEN_BASE_SPD * (1 + base_mult) # 103 * 1.39
-    lushen_rune_bonus = 0 # current rune bonus needed
-    
-    while (LUSHEN_BASE_BONUS + lushen_rune_bonus) * SPD_BUFF <= jamire_final_speed:
-        lushen_rune_bonus += 1 # calculating the minimum speed required from runes
-        
-    lushen_total = LUSHEN_BASE_SPD + lushen_rune_bonus # readable total (103 + rune bonus)
-    
-    lushen_return = (lushen_total, lushen_rune_bonus)
+
+    # --- Nuker  ---
+    NUKER__BASE_BONUS = LUSHEN_BASE_SPD * (1 + base_mult)  # 103 * 1.39
+    nuker__rune_bonus = 0  # current rune bonus needed
+
+    while (NUKER__BASE_BONUS + nuker__rune_bonus) * SPD_BUFF <= jamire_final_speed:
+        nuker__rune_bonus += 1  # calculating the minimum speed required from runes
+
+    lushen_total = LUSHEN_BASE_SPD + nuker__rune_bonus  # readable total (103 + rune bonus)
+
+    lushen_return = (lushen_total, nuker__rune_bonus)
     # --------------
 
-    return (lushen_return, homunl_return)
+    return lushen_return, homunl_return
 
-def display_for_it(jamire_bonus_speed, use_lead):
-    ((lu_t, lu_rb), (ho_t, ho_rb)) = calcSpeedTune(jamire_bonus_speed, use_lead)
-    print("You need +"+ str(ho_rb) + " speed on your light homun to reach " + str(ho_t) + " speed.")
-    print("You need +" + str(lu_rb) +" speed on your Lushen to reach " + str(lu_t) + " speed.")
-    
+
+def display_needed_speed(jamire_bonus_speed, use_lead, nuker_bs=LUSHEN_BASE_SPD):
+    ((lu_t, lu_rb), (ho_t, ho_rb)) = calc_speed_tune(jamire_bonus_speed, use_lead, nuker_bs)
+    print("You need +" + str(ho_rb) + " speed on your light homun to reach " + str(ho_t) + " speed.")
+    print("You need +" + str(lu_rb) + " speed on your Lushen to reach " + str(lu_t) + " speed.")
+
+
 # =================
 
-# Here we calc for a Jamire with a bonus of 200 from runes and using it's lead
-display_for_it(200, True)
+# Here we calc for a Jamire with a bonus of 200 from runes and using its lead
+display_needed_speed(200, True)
